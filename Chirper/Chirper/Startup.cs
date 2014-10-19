e08ec7267fad8b12a3ecf061b86041f11718a4a8
Nick S. <nicsears@iupui.edu>
2014-10-19 17:39:55 -0400

    Twitter-like Service
    This the development code for the Twitter-like Service

---
+++ Chirper/Chirper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chirper.Startup))]
+namespace Chirper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
